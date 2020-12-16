29a0d2f5daafe68ff83bd8eda736260d6f7ccda2
jamesjoel <james.joelj@gmail.com>
2020-12-16 11:38:48 +0530

    codeigniter

---
+++ ci/application/views/demo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body{
+			background-color: red;
+		}
+	</style>
+</head>
+<body>
+<h2>This is Demo Page</h2>
+</body>
+</html>
